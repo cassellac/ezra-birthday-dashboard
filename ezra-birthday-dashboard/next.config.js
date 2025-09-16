bdbe1ea0729b706428ed82516136fdcce3ec9714
Anthony <acinktown@gmail.com>
2025-09-16 11:43:38 -0700

    Delete ezra-birthday-dashboard directory
    Signed-off-by: Anthony <acinktown@gmail.com>

--- ezra-birthday-dashboard/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['source.unsplash.com', 'apod.nasa.gov', 'api.nasa.gov'],
-  },
-}
-
-module.exports = nextConfig
